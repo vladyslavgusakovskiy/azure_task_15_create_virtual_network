18f6b9485f02b006d5f0f49f2a45a03cde52f102
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-10-16 21:20:21 +0300

    Added a check to determine if a VNet with the same name already exists in the RG

--- task.ps1
+++ task.ps1
@@ -13,8 +13,25 @@ $mngSubnetIpRange = "10.20.30.128/26"
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-Write-Host "Creating a virtual network ..."
-$webservers = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange
-$database = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange
-$management = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange
-New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $webservers ,$database, $management
+Write-Host "Creating subnets ..."
+$webserversSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange
+$databaseSubnet = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange
+$managementSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange
+
+$subnetConfigs = $webserversSubnet, $databaseSubnet, $managementSubnet
+
+$vnet = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
+
+if ($null -eq $vnet) {
+    Write-Host "Virtual Network $virtualNetworkName not found. Creating a new VNet..."
+    
+    New-AzVirtualNetwork -Name $virtualNetworkName `
+        -ResourceGroupName $resourceGroupName `
+        -Location $location `
+        -AddressPrefix $vnetAddressPrefix `
+        -Subnet $subnetConfigs
+    
+    Write-Host "Virtual Network $virtualNetworkName created successfully."
+} else {
+    Write-Host "Virtual Network $virtualNetworkName already exists. Skipping creation."
+}
