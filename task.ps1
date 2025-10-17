4e4703859f0af4ba68766542ba390831db0ba1a8
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-10-17 16:13:55 +0300

    final solution

--- task.ps1
+++ task.ps1
@@ -17,21 +17,57 @@ Write-Host "Creating subnets ..."
 $webserversSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange
 $databaseSubnet = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange
 $managementSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange
-
 $subnetConfigs = $webserversSubnet, $databaseSubnet, $managementSubnet
 
 $vnet = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
 
-if ($null -eq $vnet) {
-    Write-Host "Virtual Network $virtualNetworkName not found. Creating a new VNet..."
-    
+if (-not $vnet) {
+    Write-Host "Virtual Network $virtualNetworkName not found. Creating a new VNet ..."
+
     New-AzVirtualNetwork -Name $virtualNetworkName `
         -ResourceGroupName $resourceGroupName `
         -Location $location `
         -AddressPrefix $vnetAddressPrefix `
         -Subnet $subnetConfigs
-    
+
     Write-Host "Virtual Network $virtualNetworkName created successfully."
 } else {
-    Write-Host "Virtual Network $virtualNetworkName already exists. Skipping creation."
+    if ($vnet.AddressSpace.AddressPrefixes -notcontains "10.20.30.0/24") {
+        $vnet.AddressSpace.AddressPrefixes += "10.20.30.0/24"
+        Set-AzVirtualNetwork -VirtualNetwork $vnet
+    } else {
+        Write-Host "Virtual Network Address Prefix $vnetAddressPrefix already exists. Verifying subnets ..."
+        $existingSubnetNames = $vnet.Subnets | ForEach-Object {$_.Name}
+
+        $added = $false
+
+        if ($existingSubnetNames -notcontains $webSubnetName) {
+            Write-Host "Adding missing subnet: $webSubnetName"
+            Add-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange -VirtualNetwork $vnet | Out-Null
+            $added = $true
+        } else {
+            Write-Host "Subnet $webSubnetName already exists. Skipping."
+        }
+
+        if ($existingSubnetNames -notcontains $dbSubnetName) {
+            Write-Host "Adding missing subnet: $dbSubnetName"
+            Add-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange -VirtualNetwork $vnet | Out-Null
+            $added = $true
+        } else {
+            Write-Host "Subnet $dbSubnetName already exists. Skipping."
+        }
+
+        if ($existingSubnetNames -notcontains $mngSubnetName) {
+            Write-Host "Adding missing subnet: $mngSubnetName"
+            Add-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange -VirtualNetwork $vnet | Out-Null
+            $added = $true
+        } else {
+            Write-Host "Subnet $mngSubnetName already exists. Skipping."
+        }
+
+        if ($added) {
+            Set-AzVirtualNetwork -VirtualNetwork $vnet | Out-Null
+            Write-Host "Persisted subnet additions to VNet."
+        }
+    }
 }
